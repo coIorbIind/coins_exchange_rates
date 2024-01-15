df00c26306c9f9dce19b4c8e2aca822a6afb974d
colorblind <colorblind.backend.work@gmail.com>
2024-01-15 21:55:28 +0300

    Add logger

---
+++ src/coins_exchange_rates/core/logger.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+from config.settings import settings
+
+
+def init_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(settings.LOG_FILE_PATH, mode='a')
+    std_handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+
+    file_handler.setFormatter(formatter)
+    std_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(std_handler)
+
+    return logger
